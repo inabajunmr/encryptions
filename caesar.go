04eeb1a8c36c940f47fe362515e83a4d9e926db1
inabajunmr <inabajun.for.regi@gmail.com>
2019-12-07 14:23:35 +0900

    caesar

---
+++ caesar.go
@@ -0,0 +1,103 @@
+package enctice
+
+import (
+	"errors"
+)
+
+// Encrypt by caesar
+func Encrypt(v string, key int) (string, error) {
+	result := []rune{}
+	for _, r := range v {
+		if isLowerCase(r) {
+			result = append(result, encryptLowerCase(r, key))
+			continue
+		}
+
+		if isUpperCase(r) {
+			result = append(result, encryptUpperCase(r, key))
+			continue
+		}
+
+		if isSpace(r) {
+			result = append(result, r)
+			continue
+		}
+
+		return "", errors.New("this method can encrypt only [a-zA-Z]")
+	}
+
+	return string(result), nil
+}
+
+// Decrypt by caesar
+func Decrypt(v string, key int) (string, error) {
+	result := []rune{}
+	for _, r := range v {
+		if isLowerCase(r) {
+			result = append(result, decryptLowerCase(r, key))
+			continue
+		}
+
+		if isUpperCase(r) {
+			result = append(result, decryptUpperCase(r, key))
+			continue
+		}
+
+		if isSpace(r) {
+			result = append(result, r)
+			continue
+		}
+
+		return "", errors.New("this method can encrypt only [a-zA-Z]")
+	}
+
+	return string(result), nil
+}
+
+func encryptLowerCase(v rune, key int) rune {
+	sum := int(v) + key
+	if sum < 122 {
+		return rune(sum)
+	}
+
+	return rune((sum)%122 + 96)
+}
+
+func encryptUpperCase(v rune, key int) rune {
+	sum := int(v) + key
+	if sum < 90 {
+		return rune(sum)
+	}
+
+	return rune((sum)%90 + 64)
+}
+
+func decryptLowerCase(v rune, key int) rune {
+	diff := int(v) - key
+	if diff > 96 {
+		return rune(diff)
+	}
+
+	return rune(122 - (96 - diff))
+}
+
+func decryptUpperCase(v rune, key int) rune {
+	diff := int(v) - key
+	if diff > 64 {
+		return rune(diff)
+	}
+
+	return rune(90 - (64 - diff))
+}
+
+func isLowerCase(v rune) bool {
+	return v >= 97 && v <= 122
+}
+
+func isUpperCase(v rune) bool {
+	return v >= 65 && v <= 90
+}
+
+func isSpace(v rune) bool {
+	return v == 32
+}
---
+++ caesar_test.go
@@ -0,0 +1,86 @@
+package enctice
+
+import "testing"
+
+func TestEncrypt_KeyIs0(t *testing.T) {
+	result, err := Encrypt("ABC def", 0)
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+
+	if result != "ABC def" {
+		t.Fatalf("failed test. result is %v", result)
+	}
+}
+
+func TestEncrypt_KeyIsNot0(t *testing.T) {
+	// key is 1
+	result, err := Encrypt("ABC def", 1)
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+	if result != "BCD efg" {
+		t.Fatalf("failed test. result is %v", result)
+	}
+
+	// key is 10
+	result, err = Encrypt("vwx XYZ", 10)
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+	if result != "fgh HIJ" {
+		t.Fatalf("failed test. result is %v", result)
+	}
+}
+
+func TestEncrypt_InvalidValue(t *testing.T) {
+	// key is 1
+	_, err := Encrypt("ABC def 123", 1)
+	if err == nil {
+		t.Fatalf("failed test")
+	}
+}
+
+// exception case
+
+func TestDecrypt_KeyIs0(t *testing.T) {
+	c, _ := Encrypt("ABC def", 0)
+	result, err := Decrypt(c, 0)
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+	if result != "ABC def" {
+		t.Fatalf("failed test. result is %v", result)
+	}
+}
+
+func TestDecrypt_KeyIsNot0(t *testing.T) {
+	// key is 1
+	c, _ := Encrypt("ABC def", 1)
+	result, err := Decrypt(c, 1)
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+	if result != "ABC def" {
+		t.Fatalf("failed test. result is %v", result)
+	}
+
+	// key is 10
+	c, _ = Encrypt("vwx XYZ", 10)
+	result, err = Decrypt(c, 10)
+
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+	if result != "vwx XYZ" {
+		t.Fatalf("failed test. result is %v", result)
+	}
+}
+
+func TestDecrypt_InvalidValue(t *testing.T) {
+	// key is 1
+	_, err := Decrypt("ABC def 123", 1)
+	if err == nil {
+		t.Fatalf("failed test")
+	}
+}
