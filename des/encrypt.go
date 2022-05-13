5bf18887e2d4a941cc6c4afc82338692f0efc201
inabajunmr <inabajun.for.regi@gmail.com>
2022-05-14 01:18:05 +0900

    wip

---
+++ des/encrypt.go
@@ -0,0 +1,36 @@
+package des
+
+func Encrypt(m []byte, keys []byte) []byte {
+	// step 1
+	n := 1
+
+	// step 2
+	ip := ip(m)
+
+	// step 3
+	l0 := ip[:32]
+	r0 := ip[32:]
+
+	// step 4
+	// TODO
+
+}
+
+var IP1_ARRAY = []int{
+	58, 50, 42, 34, 26, 18, 10, 2,
+	60, 52, 44, 36, 28, 20, 12, 4,
+	62, 54, 46, 38, 30, 22, 14, 6,
+	64, 56, 48, 40, 32, 24, 16, 8,
+	57, 49, 41, 33, 25, 17, 9, 1,
+	59, 51, 43, 35, 27, 19, 11, 3,
+	61, 53, 45, 37, 29, 21, 13, 5,
+	63, 55, 47, 39, 31, 23, 15, 7,
+}
+
+func ip(key []byte) []byte {
+	var bits []bool
+	for i := 0; i < 64; i++ {
+		bits = append(bits, bit(IP1_ARRAY[i], key))
+	}
+	return bitsToBytes(bits)
+}
